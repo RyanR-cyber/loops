69f4f62d8a7ebf3c6e903215cdab92f7ad5b52a3
RyanR-cyber <ozzy19992@gmail.com>
2025-09-21 20:23:18 -0500

    intlize

---
+++ app.js
@@ -0,0 +1,29 @@
+const cars = ['chevy', 'ford', 'honda', 'toyota'];
+
+for (name of cars) {
+    console.log(name);
+    if (name === 'honda') {
+        console.log('we found the Honda!');
+        break;
+    }
+} // For Loop
+
+let i = 5;
+let j = 10;
+
+while (i <=6) {
+    i += j;
+    i++;
+   
+}
+console.log("Total is:"+ j); // While Loop
+    
+const text = document.querySelector(".title");
+
+text.classList.add("change"); //Change css with JS
+
+onsole.log("Total is:"+ j); // While Loop
+    
+const text = document.querySelector(".title");
+
+text.classList.add("change"); //Change css with JS
